9630055482872e3851bafb68a2401cb56b6da908
unknown-neko <unknown.neko.mail@gmail.com>
2023-11-11 13:30:32 +0900

    patch static exportの部分を再度追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 // 参考：https://nextjs.org/docs/pages/building-your-application/routing/internationalization
 const nextConfig = {
+  output: 'export',
 }
 
 module.exports = nextConfig
